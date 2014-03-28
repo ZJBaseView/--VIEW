4a7c35eb814720afaeac8ca33eb912873b1e043e
ZJBaseView <zjdwy@126.com>
2014-03-28 13:16:45 +0800

    Revert "Revert "弹出VIEW""
    This reverts commit 62559f20d513a4d7fd110d74f199ea107aa326bc.

---
+++ POPView/ZJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZJAppDelegate.h
+//  POPView
+//
+//  Created by 郭剑聪 on 14-3-27.
+//  Copyright (c) 2014年 ZHAO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ POPView/ZJViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZJViewController.h
+//  POPView
+//
+//  Created by 郭剑聪 on 14-3-27.
+//  Copyright (c) 2014年 ZHAO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZJViewController : UIViewController
+
+@end
